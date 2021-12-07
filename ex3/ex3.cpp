fc1b045be098fa2d43c19ed22ba2bcc8f1f46b9c
Andrey <89473462+Arty525@users.noreply.github.com>
2021-12-07 16:45:58 +0300

    Add files via upload

---
+++ ex3/ex3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int snake[5][5];
+	int n = 0;
+		for (int x = 0; x < 5; ++x) {
+			for (int y = 0; y < 5 && (x == 0 || x == 2 || x == 4); ++y) {
+				snake[x][y] = n;
+				cout << snake[x][y];
+				++n;
+			}
+			for (int y = 0; y < 5 && (x == 1 || x == 3); ++y) {
+				snake[x][4 - y] = n;
+				cout << snake[x][4 - y];
+				++n;
+			}
+			cout << endl;
+		}
+	return 0;
+}
