091eee1ff0f86f59316e74b57c3774eb3c06c958
Andrey <89473462+Arty525@users.noreply.github.com>
2021-12-08 13:46:15 +0300

    Add files via upload

--- ex5/ex5.cpp
+++ ex5/ex5.cpp
@@ -27,10 +27,6 @@ int main()
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < 4; ++j) {
 			if (i != j) matrix[i][j] = 0;
-		}
-	}
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
 			cout << matrix[i][j] << " ";
 		}
 		cout << endl;
