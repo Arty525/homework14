669173f235161a0649cd274d6db8690fc5dd2763
Andrey <89473462+Arty525@users.noreply.github.com>
2021-12-09 14:47:58 +0300

    Add files via upload

--- ex7/ex7.cpp
+++ ex7/ex7.cpp
@@ -3,12 +3,14 @@
 
 using namespace std;
 
-void pop(bool pimple[12][12], int a, int b, int x, int y, int bubbles) {
+int bubbles = 144;
+void pop(bool pimple[12][12], int a, int b, int x, int y) {
 	for (int i = a; i <= x; ++i) {
 		for (int j = b; j <= y; ++j) {
 			if (pimple[i][j]) {
 				cout << "POP! ";
 				pimple[i][j] = false;
+				--bubbles;
 			}
 			else {
 				cout << "X ";
@@ -32,21 +34,29 @@ int main() {
 						 { true, true, true, true, true, true, true, true, true, true, true, true },
 						 { true, true, true, true, true, true, true, true, true, true, true, true } };
 	int a, b, x, y;
-	int bubbles = 0;
-	while (bubbles != 144) {
+	while (bubbles != 0) {
 		for (int i = 0; i < 12; ++i) {
 			for (int j = 0; j < 12; ++j) {
 				cout << (pimple[i][j] ? "O" : "X") << " ";
 			}
 			cout << endl;
 		}
-		cout << "Enter the boundaries of the region." << endl << "start: ";
+		cout << "=====================" << endl;
+		cout << "Enter the boundaries of the region." << endl << "start: " << endl;
 		cin >> a;
 		cin >> b;
-		cout << "end: ";
+		if (a < 0 || b < 0 || a > 11 || b > 11) {
+			cout << "Error! values are out of range.";
+			return 0;
+		}
+		cout << "end: " << endl;
 		cin >> x;
 		cin >> y;
-		pop(pimple, a, b, x, y, bubbles);
-		cout << bubbles;
+		if (x < 0 || y < 0 || x > 11 || y > 11 || x < a || y < b) {
+			cout << "Error! values are out of range.";
+			return 0;
+		}
+		pop(pimple, a, b, x, y);
+		cout << "=====================" << endl;
 	}
 }
