d473100fce4ecad3fa268a10227c951d17f7f53c
Andrey <89473462+Arty525@users.noreply.github.com>
2021-12-09 15:36:01 +0300

    Add files via upload

---
+++ ex8/ex8.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int landscape[5][5][10];
+
+	cout << "Enter block's height: " << endl;
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 5; ++j) {
+			int h;
+			cout << "block[" << i << "][" << j << "]: ";
+			cin >> h;
+			for (int x = 0; x < 10; ++x) x < h ? landscape[i][j][x] = 1 : landscape[i][j][x] = 0;
+		}
+	}
+
+	int h;
+	cout << "Enter section height: ";
+	cin >> h;
+
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 5; ++j) {
+			cout << landscape[i][j][h];
+		}
+		cout << endl;
+	}
+}
